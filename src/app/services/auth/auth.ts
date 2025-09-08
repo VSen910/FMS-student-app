f05ab1e294251d6b7269a2f6a68584178bbab673
Vineet Sen <imvsen02@gmail.com>
2025-09-09 00:29:18 +0530

    add environment.ts

--- src/app/services/auth/auth.ts
+++ src/app/services/auth/auth.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { AuthUser } from './authUser.model';
 import { Observable } from 'rxjs';
 import { CookieService } from 'ngx-cookie-service';
+import { environment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
@@ -12,16 +13,16 @@ export class Auth {
 
   login(credentials: { email: string; password: string }): Observable<AuthUser> {
     console.log('Login attempted with', credentials);
-    return this.http.post<AuthUser>('http://127.0.0.1:8080/api/auth/login/student', credentials)
+    return this.http.post<AuthUser>(`${environment.apiUrl}/api/auth/login/student`, credentials)
   }
 
   register(details: { name: string; email: string; password: string }): Observable<AuthUser> {
     console.log('Registering user with', details);
-    return this.http.post<AuthUser>('http://127.0.0.1:8080/api/auth/register', details);
+    return this.http.post<AuthUser>(`${environment.apiUrl}/api/auth/register`, details);
   }
 
   validateToken(): Observable<boolean> {
     const token = this.cookieService.get('token');
-    return this.http.get<boolean>('http://127.0.0.1:8080/api/auth/validate', { headers: { Authorization: `Bearer ${token}` } });
+    return this.http.get<boolean>(`${environment.apiUrl}/api/auth/validate`, { headers: { Authorization: `Bearer ${token}` } });
   }
 }
