235d6d989f1c0474ae92641b7a49c80a675c1a61
Vineet Sen <imvsen02@gmail.com>
2025-09-16 10:39:48 +0530

    add token validation

--- src/app/feedback-page/feedback-page.ts
+++ src/app/feedback-page/feedback-page.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-import { Header } from "../components/header/header";
-import { FeedbackForm } from "../components/feedback-form/feedback-form";
+import { Component, OnInit } from '@angular/core';
+import { Header } from '../components/header/header';
+import { FeedbackForm } from '../components/feedback-form/feedback-form';
+import { Auth } from '../services/auth/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-feedback-page',
   imports: [Header, FeedbackForm],
   templateUrl: './feedback-page.html',
-  styleUrl: './feedback-page.css'
+  styleUrl: './feedback-page.css',
 })
-export class FeedbackPage{
-   
+export class FeedbackPage implements OnInit {
+  constructor(private auth: Auth, private router: Router) {}
+
+  ngOnInit(): void {
+    this.auth.validateToken().subscribe({
+      next: (isValid) => {
+        if (!isValid) {
+          this.router.navigate(['/login']);
+        }
+      },
+      error: (err) => {
+        console.error('Token validation failed', err);
+        this.router.navigate(['/login']);
+      },
+    });
+  }
 }
